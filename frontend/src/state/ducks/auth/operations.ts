59b812d577776c06cb3801eb8b27e0d26520c1f5
gtongy <32930281+gtongy@users.noreply.github.com>
2019-07-14 12:02:31 +0900

    :recycle: login google actions move to operations

--- frontend/src/state/ducks/auth/operations.ts
+++ frontend/src/state/ducks/auth/operations.ts
@@ -1,14 +1,18 @@
 import firebase from '../../../firebase/firebase';
+import Redux from 'redux';
+import actions from './actions';
 
-const loginGoogle = async () => {
+const loginGoogle = async (dispatch: Redux.Dispatch) => {
   let provider = new firebase.auth.GoogleAuthProvider();
-  return await firebase
+  const response = await firebase
     .auth()
     .signInWithPopup(provider)
     .catch(error => {
       // TODO: sign in error handling
       throw new Error(error.message);
     });
+  if (response.user === null) throw new Error('response user is not found');
+  dispatch(actions.isLogin(response.user));
 };
 
 export default { loginGoogle };
--- frontend/src/views/containers/auth/GoogleLoginContainer.ts
+++ frontend/src/views/containers/auth/GoogleLoginContainer.ts
@@ -1,15 +1,12 @@
 import { connect } from 'react-redux';
 import GoogleLoginButton from '../../components/auth/GoogleLoginButton';
 import operations from '../../../state/ducks/auth/operations';
-import actions from '../../../state/ducks/auth/actions';
 import Redux from 'redux';
 
 const mapDispatchToProps = (dispatch: Redux.Dispatch) => {
   return {
-    onClick: async () => {
-      const response = await operations.loginGoogle();
-      if (response.user === null) return;
-      dispatch(actions.isLogin(response.user));
+    onClick: () => {
+      operations.loginGoogle(dispatch);
     }
   };
 };

6bd31641ccfb0aa6453a1d21d5ecc5143973e189
Syarig <syarig110@yahoo.co.jp>
2019-07-15 17:17:08 +0900

    ニックネームを追加

--- api/entity/user.go
+++ api/entity/user.go
@@ -3,6 +3,7 @@ package entity
 // User is connthass account
 type User struct {
 	Base
-	Name      string `gorm:"size:50;not null;"`
+	Name      string `gorm:"size:50;not null;unique_index"`
+	Nickname  string `gorm:"size:50;not null"`
 	SysRoleID uint
 }

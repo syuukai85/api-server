66d1c2c419c64883ab1a8dd56b9ec7fbfe53c819
gtongy <32930281+gtongy@users.noreply.github.com>
2019-07-17 08:54:53 +0900

    append group,user chip, and append category

--- frontend/src/views/components/event/EventListItem.tsx
+++ frontend/src/views/components/event/EventListItem.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import Grid from '@material-ui/core/Grid';
+import Chip from '@material-ui/core/Chip';
+import Box from '@material-ui/core/Box';
+import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import { makeStyles } from '@material-ui/core/styles';
@@ -16,6 +19,15 @@ const useStyles = makeStyles({
   },
   eventContainer: {
     height: '200px'
+  },
+  groupChipContainer: {
+    margin: '10px'
+  },
+  userChipContainer: {
+    margin: '10px'
+  },
+  category: {
+    margin: '5px'
   }
 });
 
@@ -25,8 +37,12 @@ const EventListItem: React.FC = () => {
     <ListItem>
       <Grid container spacing={3}>
         <Grid item xs={12} sm={3}>
-          <div className={classes.date}>6/18</div>
-          <div className={classes.time}>11:00~19:00</div>
+          <Box textAlign="left" fontSize={60}>
+            6/18
+          </Box>
+          <Box textAlign="left" fontSize={17}>
+            11:00~19:00
+          </Box>
         </Grid>
         <Grid item xs={12} sm={9}>
           <Grid container>
@@ -38,7 +54,18 @@ const EventListItem: React.FC = () => {
           <Grid container>
             <Divider />
             <Grid item xs={12} sm={8}>
-              <div>aaa</div>
+              <div className={classes.groupChipContainer}>
+                <Chip avatar={<Avatar>T</Avatar>} label="takoyaki" />
+              </div>
+              <div className={classes.userChipContainer}>
+                <Chip avatar={<Avatar>G</Avatar>} label="gtongy" />
+              </div>
+              <label>category</label>
+              <div>
+                <Chip className={classes.category} label="Swift" />
+                <Chip className={classes.category} label="Go" />
+                <Chip className={classes.category} label="React" />
+              </div>
             </Grid>
             <Grid item xs={12} sm={4}>
               <img src={logo} alt="" />

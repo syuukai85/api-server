6d5c97eb8223069bbfceb3c3d533593cb693a4c3
gtongy <32930281+gtongy@users.noreply.github.com>
2019-07-20 19:30:12 +0900

    :pencil2: fix typo

--- frontend/src/firebase/config.ts
+++ frontend/src/firebase/config.ts
@@ -1,5 +1,5 @@
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_DEV_FIREBASE_APP_KEY,
+  apiKey: process.env.REACT_APP_DEV_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_DEV_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DEV_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_DEV_FIREBASE_PROJECT_ID,

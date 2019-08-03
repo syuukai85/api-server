6cbd1021c32eeded0b970665c5a7c59d7a442369
gtongy <32930281+gtongy@users.noreply.github.com>
2019-08-03 19:56:23 +0900

    :sparkles: modify container

--- frontend/src/views/components/event/EventDetail.tsx
+++ frontend/src/views/components/event/EventDetail.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Event } from 'typescript-fetch-api';
 
 interface Props {
-  id: string | null;
+  id: string;
+  event: Event;
+  isLoading: boolean;
+  error: Error;
+  searchEvent: (eventId: string) => void;
 }
 
 const EventDetail: React.FC<Props> = (props: Props) => {
+  const effectFn = () => {
+    props.searchEvent(props.id);
+  };
+  useEffect(effectFn, []);
   return <div>Event Detail</div>;
 };
 
--- frontend/src/views/containers/event/EventDetailContainer.ts
+++ frontend/src/views/containers/event/EventDetailContainer.ts
@@ -1,6 +1,9 @@
 import { connect } from 'react-redux';
+import { Event } from 'typescript-fetch-api';
 import EventDetail from '../../components/event/EventDetail';
 import * as eventSelector from '../../../state/ducks/events/selectors';
+import oparations from '../../../state/ducks/events/operations';
+import Redux from 'redux';
 
 interface State {
   router: {
@@ -8,12 +11,31 @@ interface State {
       pathname: string;
     };
   };
+  eventsState: {
+    event: {
+      event: Event;
+      isLoading: boolean;
+      error: Error;
+    };
+  };
 }
 
 const mapStateToProps = (state: State) => {
   return {
-    id: eventSelector.getEventIdFromRouter(state)
+    id: eventSelector.getEventIdFromRouter(state),
+    event: state.eventsState.event.event,
+    isLoading: state.eventsState.event.isLoading,
+    error: state.eventsState.event.error
   };
 };
 
-export default connect(mapStateToProps)(EventDetail);
+const mapDispatchToProps = (dispatch: Redux.Dispatch) => {
+  return {
+    searchEvent: (eventId: string) => oparations.searchEvent(dispatch, eventId)
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(EventDetail);

1045e8c7caa4dcf36c42b2a1855db8b1f695f57c
gtongy <32930281+gtongy@users.noreply.github.com>
2019-08-09 22:19:03 +0900

    :sparkles: append searchGroup operations

--- frontend/src/state/ducks/groups/operations.ts
+++ frontend/src/state/ducks/groups/operations.ts
@@ -1,8 +1,12 @@
 import Redux from 'redux';
 import actions from './actions';
 
+const searchGroup = (dispatch: Redux.Dispatch, groupId: string) => {
+  dispatch(actions.searchGroup.searchGroup(groupId));
+};
+
 const searchRecentlyAddedGroup = (dispatch: Redux.Dispatch) => {
   dispatch(actions.searchRecentlyAddedGroup.searchRecentlyAddedGroup());
 };
 
-export default { searchRecentlyAddedGroup };
+export default { searchGroup, searchRecentlyAddedGroup };

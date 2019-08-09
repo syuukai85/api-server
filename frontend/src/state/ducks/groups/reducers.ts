6a9db87600d46f2474c9069637a95628cd053981
gtongy <32930281+gtongy@users.noreply.github.com>
2019-08-09 22:25:28 +0900

    :sparkles: group reducer append

--- frontend/src/state/ducks/groups/reducers.ts
+++ frontend/src/state/ducks/groups/reducers.ts
@@ -1,7 +1,40 @@
 import { combineReducers } from 'redux';
 import { Group } from 'typescript-fetch-api';
-import { SearchRecentlyAddedGroupAction } from './actions';
+import { SearchGroupAction, SearchRecentlyAddedGroupAction } from './actions';
 import { ActionTypes } from './types';
+
+interface GroupState {
+  group: Group;
+  isLoading: boolean;
+}
+
+const group = (
+  state: GroupState = { group: {}, isLoading: true },
+  action: SearchGroupAction
+): GroupState => {
+  switch (action.type) {
+    case ActionTypes.REQUEST_GROUP: {
+      return Object.assign({}, state, {
+        isLoading: action.isLoading
+      });
+    }
+    case ActionTypes.SUCCESS_GROUP: {
+      return Object.assign({}, state, {
+        group: action.group,
+        isLoading: action.isLoading
+      });
+    }
+    case ActionTypes.ERROR_GROUP: {
+      return Object.assign({}, state, {
+        isLoading: action.isLoading,
+        error: action.error
+      });
+    }
+    default: {
+      return state;
+    }
+  }
+};
 
 interface RecentlyAddedGroupState {
   groups: Array<Group>;
@@ -36,5 +69,5 @@ const recentlyAddedGroup = (
   }
 };
 
-const groupsReducer = combineReducers({ recentlyAddedGroup });
+const groupsReducer = combineReducers({ group, recentlyAddedGroup });
 export default groupsReducer;

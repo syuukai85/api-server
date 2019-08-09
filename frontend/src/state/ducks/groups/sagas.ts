9139cc3db761be3d74ff5e9c9fcc1fb12c69f457
gtongy <32930281+gtongy@users.noreply.github.com>
2019-08-09 22:30:28 +0900

    :sparkles: searchGroup sagas append

--- frontend/src/state/ducks/groups/sagas.ts
+++ frontend/src/state/ducks/groups/sagas.ts
@@ -1,19 +1,32 @@
 import { takeEvery, call, put } from 'redux-saga/effects';
-import actions from './actions';
+import { default as actions, SearchGroupAction } from './actions';
 import { GroupApi, SearchGroupsRequest } from 'typescript-fetch-api';
 import { ActionTypes } from './types';
 
 let api = new GroupApi();
 
+const searchGroups = (req: SearchGroupsRequest) => {
+  return api
+    .searchGroups(req)
+    .then(groups => groups)
+    .catch(error => {
+      throw new Error(error);
+    });
+};
+
+function* searchGroup(action: SearchGroupAction) {
+  try {
+    // TODO: 検索条件のformatをどうする？
+    const group = yield call(searchGroups, {
+      query: `groupId:${action.groupId}`
+    });
+    yield put(actions.searchGroup.searchSuccessGroup(group));
+  } catch (error) {
+    yield put(actions.searchGroup.searchErrorGroup(error));
+  }
+}
+
 function* searchRecentlyAddedGroup() {
-  const searchGroups = (req: SearchGroupsRequest) => {
-    return api
-      .searchGroups(req)
-      .then(groups => groups)
-      .catch(error => {
-        throw new Error(error);
-      });
-  };
   try {
     const groups = yield call(searchGroups, { perPage: 5 });
     yield put(
@@ -27,6 +40,7 @@ function* searchRecentlyAddedGroup() {
 }
 
 const sagas = [
+  takeEvery(ActionTypes.REQUEST_GROUP, searchGroup),
   takeEvery(ActionTypes.REQUEST_NEWLY_GROUP, searchRecentlyAddedGroup)
 ];
 export default sagas;

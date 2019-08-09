4e52ff4a4978827d15882bf0917c5dbd19c5ed82
gtongy <32930281+gtongy@users.noreply.github.com>
2019-08-09 22:19:58 +0900

    :sparkles: append actions searchGroup import & export

--- frontend/src/state/ducks/groups/actions/index.ts
+++ frontend/src/state/ducks/groups/actions/index.ts
@@ -1,9 +1,12 @@
+import { default as searchGroup, SearchGroupAction } from './searchGroup';
 import {
   default as searchRecentlyAddedGroup,
   SearchRecentlyAddedGroupAction
 } from './searchRecentlyAddedGroup';
+export type SearchGroupAction = SearchGroupAction;
 export type SearchRecentlyAddedGroupAction = SearchRecentlyAddedGroupAction;
 
 export default {
+  searchGroup,
   searchRecentlyAddedGroup
 };

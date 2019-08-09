da6b09ff5b1f5bca23ce50c62c38dc315999bb60
gtongy <32930281+gtongy@users.noreply.github.com>
2019-08-10 00:47:42 +0900

    :sparkles: presentational component append useEffect

--- frontend/src/views/components/group/GroupDetail.tsx
+++ frontend/src/views/components/group/GroupDetail.tsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Group } from 'typescript-fetch-api';
 
-const GroupDetail: React.FC = () => {
+interface Props {
+  id: string;
+  group: Group;
+  isLoading: boolean;
+  error: Error;
+  searchGroup: (groupId: string) => void;
+}
+
+const GroupDetail: React.FC<Props> = (props: Props) => {
+  const effectFn = () => {
+    props.searchGroup(props.id);
+  };
+  useEffect(effectFn, []);
   return (
     <>
       <div>group detail</div>

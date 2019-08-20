56834ed1ab9018de0ac18b3d644cc62f64b90b98
gtongy <32930281+gtongy@users.noreply.github.com>
2019-08-20 23:54:02 +0900

    update group reducks doc comment

--- frontend/src/state/ducks/groups/selectors.ts
+++ frontend/src/state/ducks/groups/selectors.ts
@@ -15,11 +15,21 @@ interface Param {
   id: string;
 }
 
+/**
+ * 渡された情報からrouterをselectする関数
+ *
+ * @param {State} state state情報
+ */
 const routerSelector = (state: State): Router => state.router;
 
+/**
+ * 渡された情報からgroup idをselectする関数
+ *
+ * @param {Router} state state情報
+ */
 const getGroupIdFromRouter = createSelector(
   routerSelector,
-  state => {
+  (state: Router): string => {
     const match = matchPath<Param>(state.location.pathname, {
       path: '/groups/:id',
       exact: true,

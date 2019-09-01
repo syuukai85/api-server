4aea66dd82d599177d1e57bb27d1203e6b2ffe25
gtongy <32930281+gtongy@users.noreply.github.com>
2019-09-01 19:07:42 +0900

    :recycle: replace to form validates

--- frontend/src/views/forms/addEvent.ts
+++ frontend/src/views/forms/addEvent.ts
@@ -6,73 +6,58 @@ import * as validates from './validates';
 // TODO: 選択されたlabelがいらない。削除して対象のカラムに対してnumberのみを保持するようにする
 type SelectValue = { value: number; label: string };
 
-export type AddEventFormInitValues = {
-  capacity: number;
-  categories: SelectValue[];
-  colorCode: string;
+export type FormValues = {
+  title: string;
   description: string;
-  group: SelectValue;
-  holdStartDate: Date;
-  holdEndDate: Date;
+  colorCode: string;
   imageFile: File;
-  organizers: SelectValue[];
   qrCodeFile: File;
+  capacity: number;
   recruitStartDate: Date;
   recruitEndDate: Date;
-  title: string;
+  holdStartDate: Date;
+  holdEndDate: Date;
+  organizers: SelectValue[];
   venue: SelectValue;
+  categories: SelectValue[];
+  group: SelectValue;
 };
 
-export type FormValues = {
-  capacity: number;
-  categories: SelectValue[];
-  colorCode: string;
-  description: string;
-  group: SelectValue;
-  holdStartDate: Date;
-  holdEndDate: Date;
-  imageFile: File;
-  organizers: SelectValue[];
-  qrCodeFile: File;
-  recruitStartDate: Date;
-  recruitEndDate: Date;
-  title: string;
-  venue: SelectValue;
-};
+export type AddEventFormInitValues = FormValues;
 
 const mapPropsToValues = (props: AddEventFormInitValues) => ({
-  capacity: props.capacity || 0,
-  categories: props.categories || [],
-  colorCode: props.colorCode || '#F9F9F9',
+  title: props.title || '',
   description: props.description || '',
-  group: props.group || null,
-  holdStartDate: props.holdStartDate || null,
-  holdEndDate: props.holdEndDate || null,
+  colorCode: props.colorCode || '#F9F9F9',
   imageFile: props.imageFile || null,
-  organizers: props.organizers || [],
   qrCodeFile: props.qrCodeFile || null,
+  capacity: props.capacity || 0,
   recruitStartDate: props.recruitStartDate || null,
   recruitEndDate: props.recruitEndDate || null,
-  title: props.title || '',
-  venue: props.venue || null
+  holdStartDate: props.holdStartDate || null,
+  holdEndDate: props.holdEndDate || null,
+  organizers: props.organizers || [],
+  venue: props.venue || null,
+  categories: props.categories || [],
+  group: props.group || null
 });
 
 const validateSchema = Yup.object().shape({
-  capacity: validates.numberRange('募集人数', constants.validate.capacity.min, constants.validate.capacity.max),
-  colorCode: validates.colorCode('ヘッダー画像のカラー設定'),
-  description: validates.stringMinLength('説明', constants.validate.description.minLength),
-  holdStartDate: validates.startDate('開催開始日時'),
-  holdEndDate: validates.endDate('開催終了日時', 'holdStartDate'),
-  imageFile: validates.file('ヘッダー画像'),
-  organizers: validates.organizers(),
-  qrCodeFile: validates.file('投げ銭QRコード画像'),
-  recruitStartDate: validates.startDate('募集開始日時'),
-  recruitEndDate: validates.endDate('募集終了日時', 'recruitStartDate'),
   title: validates.stringLengthRange(
     'イベント名',
     constants.validate.title.minLength,
     constants.validate.title.maxLength
   ),
+  description: validates.stringMinLength('説明', constants.validate.description.minLength),
+  colorCode: validates.colorCode('ヘッダー画像のカラー設定'),
+  imageFile: validates.file('ヘッダー画像'),
+  qrCodeFile: validates.file('投げ銭QRコード画像'),
+  capacity: validates.numberRange('募集人数', constants.validate.capacity.min, constants.validate.capacity.max),
+  recruitStartDate: validates.startDate('募集開始日時'),
+  recruitEndDate: validates.endDate('募集終了日時', 'recruitStartDate'),
+  holdStartDate: validates.startDate('開催開始日時'),
+  holdEndDate: validates.endDate('開催終了日時', 'holdStartDate'),
+  organizers: validates.organizers(),
   venue: validates.venue()
 });
 

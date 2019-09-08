2f9974ad56f2588d09a9a0ba215fb7f2f4eaf901
gtongy <32930281+gtongy@users.noreply.github.com>
2019-09-09 00:09:33 +0900

    :recycle: rename add event form container

--- frontend/src/views/scenes/event/AddScene.tsx
+++ frontend/src/views/scenes/event/AddScene.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import AddEventContainer from '../../containers/event/AddEventContainer';
+import AddEventFormContainer from '../../containers/event/AddEventFormContainer';
 import NotificationContainer from '../../containers/notification/NotificationContainer';
 import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
@@ -15,7 +15,7 @@ const AddEventScene: React.FC = () => {
   const classes = useStyles({});
   return (
     <Container className={classes.container}>
-      <AddEventContainer />
+      <AddEventFormContainer />
       <NotificationContainer />
     </Container>
   );
